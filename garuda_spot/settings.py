f4debc491416ab97c5ba95b413755d3720779f13
XxymbolL <rifqy.Kurniawan1703@gmail.com>
2025-10-22 08:39:30 +0700

    add template,url,static,views modify dataset

--- garuda_spot/settings.py
+++ garuda_spot/settings.py
@@ -45,10 +45,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'news'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -62,7 +64,7 @@ ROOT_URLCONF = 'garuda_spot.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -140,7 +142,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+if DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static' # merujuk ke /static root project pada mode development
+    ]
+else:
+    STATIC_ROOT = BASE_DIR / 'static' # merujuk ke /static root project pada mode production
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- news/models.py
+++ news/models.py
@@ -3,19 +3,13 @@ from django.db import models
 
 class News(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=300)
+    category = models.CharField(max_length=100)
+    publish_date = models.CharField(max_length=100, blank=True)
     content = models.TextField()
-    news_views = models.PositiveIntegerField(default=0)
-    created_at = models.DateTimeField(auto_now_add=True)
-    is_featured = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["-id"]
 
     def __str__(self):
         return self.title
-
-    @property
-    def is_news_hot(self):
-        return self.news_views > 500
-
-    def increment_views(self):
-        self.news_views += 1
-        self.save()
