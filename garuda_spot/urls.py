c613929e51a150d324bc47889cc5785385dc975a
Dyablouz <fernando123lawrence@gmail.com>
2025-10-22 16:50:04 +0700

    Edit main, views, models, urls

--- garuda_spot/urls.py
+++ garuda_spot/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("news.urls", namespace="news")),
     path("accounts/", include("accounts.urls", namespace="accounts")),
+    path("merch/", include("merch.urls", namespace="merch")),
 ]
--- merch/models.py
+++ merch/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Merch(models.Model):
+    CATEGORY_CHOICES = [
+        ('cap', 'Cap'),
+        ('hoodie', 'Hoodie'),
+        ('jacket', 'Jacket'),
+        ('jersey', 'Jersey'),
+        ('keychain', 'Keychain'),
+        ('scarf', 'Scarf'),
+    ]
+
+    name = models.CharField(max_length=255)
+    price = models.PositiveIntegerField(default=0)
+    stock = models.PositiveIntegerField(default=0)
+    description = models.TextField()
+    thumbnail = models.URLField()
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='update')
+    
+    def __str__(self):
+        return self.name
