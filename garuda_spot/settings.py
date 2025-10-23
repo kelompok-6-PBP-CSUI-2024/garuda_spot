831773bcd513fa03db7002568c7ddc6a22ad14ec
pit135 <wermasaubunpetrus@gmail.com>
2025-10-23 21:55:33 +0700

    perbaikan

--- garuda_spot/settings.py
+++ garuda_spot/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'forum'
+    'forum',
     'django.contrib.humanize',
     'news',
     'merch',
